8eb3af37588a9793cb091b7b992018f477da64c3
Mirza Ali Hussain <mirzaalihussain@Admins-MacBook-Pro.local>
2024-04-22 12:15:31 +0100

    Feat: add contact with response params

--- app/root.tsx
+++ app/root.tsx
@@ -1,12 +1,31 @@
+import type { LinksFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
 import {
   Form,
+  Link,
   Links,
   Meta,
+  Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData
 } from "@remix-run/react";
 
+import appStylesHref from "./app.css?url";
+import { getContacts } from "./data"
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: appStylesHref },
+];
+
+export const loader = async () => {
+  const contacts = await getContacts();
+  return json({ contacts })
+};
+
 export default function App() {
+  const { contacts } = useLoaderData<typeof loader>();
+
   return (
     <html lang="en">
       <head>
@@ -34,19 +53,47 @@ export default function App() {
             </Form>
           </div>
           <nav>
-            <ul>
+            {contacts.length ? (
+              <ul>
+                {contacts.map((contact) => (
+                  <li key={contact.id}>
+                    <Link to={`contacts/${contact.id}`}>
+                      {contact.first || contact.last ? (
+                        <>
+                          {contact.first} {contact.last}
+                        </>
+                      ) : (
+                        <i>No Name</i>
+                      )}{" "}
+                      {contact.favorite ? (
+                        <span>★</span>
+                      ) : null}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p>
+                <i>No contacts</i>
+              </p>
+            )}
+            {/* <ul>
               <li>
-                <a href={`/contacts/1`}>Your Name</a>
+                <Link to={`/contacts/1`}>Your Name</Link>
               </li>
               <li>
-                <a href={`/contacts/2`}>Your Friend</a>
+                <Link to={`/contacts/2`}>Your Friend</Link>
               </li>
-            </ul>
+            </ul> */}
           </nav>
         </div>
 
         <ScrollRestoration />
         <Scripts />
+
+        <div id="detail">
+          <Outlet />
+        </div>
       </body>
     </html>
   );
